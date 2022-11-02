a171c268bd4922bf5dae1e0ef2d4219c231a080c
pravin <pravin@Dell545>
2022-11-02 20:18:44 +0530

    Hello World Program

---
+++ Hello/src/com/programs/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.programs;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World.");
+		System.out.println("My Name Is Pravin");
+
+	
+	}
+
+}
