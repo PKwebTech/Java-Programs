d8363241c44c5fe8c6a541eefe220ac877c73ccb
pravin <pravin@Dell545>
2022-11-02 20:09:44 +0530

    Datatype

---
+++ DataTypes/src/com/programs/DataType.java
@@ -0,0 +1,55 @@
+package com.programs;
+public class DataType {
+
+	public static void main(String[] args) {
+	   // byte
+	   byte b;
+	   b=127;
+	   System.out.println("byte : "+b);
+	   
+	   // short
+	   short range;
+	   range=-127;
+	   System.out.println("Short :" +range);
+	   
+	   // int
+	   
+	   int a;
+	   a=2000000000;
+	   System.out.println("Int : "+a);
+	   
+	   // long
+	   
+	   long l;
+	   l=20000l;
+	   System.out.println("Long :"+l);
+	   
+	   // float
+	   
+	   float f;
+	   f=300.4f;
+	   System.out.println("Float :"+f);
+	   
+	   //Double
+	   
+	   double d;
+	   d=30000.5;
+	   System.out.println("Double :"+d);
+	   
+	   // char 
+	   
+	   char c;
+	   c='\u0041';
+	   System.out.println("Char :"+c);
+	   
+	   // boolean
+	   
+	   boolean bool;
+	   bool=true;
+	   System.out.println("Boolean "+bool);
+	   
+	    
+
+	}
+
+}
---
+++ DataTypes/src/com/programs/DataTypes.java
@@ -0,0 +1,42 @@
+package com.programs;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declared the variable;
+		
+		byte bval;
+		short sval;
+		int intval;
+		long lval;
+        float fval;
+        double dval;
+        char cval;
+        boolean boolval;
+        
+        // assign value to the variable 
+        
+        bval=2;
+        sval=10;
+        intval=100;
+        lval=200l;
+        fval=10.10f;
+        dval=200.10d;
+        cval='P';
+        boolval=true;
+        
+        System.out.println("Byte Value = "+bval);
+        System.out.println("Short Value = "+sval);
+        System.out.println("Integer Value = "+intval);
+        System.out.println("Long Value = "+lval);
+        System.out.println("Float Value = "+fval);
+        System.out.println("Double Value = "+dval);
+        System.out.println("Char Value = "+cval);
+        System.out.println("Boolean Value = "+boolval);
+       
+    
+	}
+
+}
